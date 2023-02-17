a871f6e9a896635e1e69883c55742601bb028c88
maitreya-v <102204411+maitreya-v@users.noreply.github.com>
2023-02-17 14:04:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import  Carousel  from "./components/UncontrolledExample";
+import NavbarHome from "./components/NavbarHome";
+import UncontrolledExample from "./components/UncontrolledExample";
+
+
+function App() {
+  return (
+    <div className="App">
+    <NavbarHome></NavbarHome>
+    <UncontrolledExample/>
+    hi``
+    </div>
+  );
+}
+
+export default App;
